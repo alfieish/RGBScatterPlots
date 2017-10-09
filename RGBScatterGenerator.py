a63ede633e626242b8d0968df373f6e03db769e1
alfieish <mauriceabrooks@gmail.com>
2017-10-09 22:30:22 +0100

    Add files via upload

---
+++ RGBScatterGenerator.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import random
+
+piclocation = r'C:\Users\Maurice\Desktop\Python\PictureTools\RGB Fingerprints\SandysMaryMagdalene.jpg'
+im = Image.open(piclocation)
+
+pixels = list(im.getdata())
+pixel_sample = random.sample(pixels, 5000)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+
+for i in pixel_sample:
+    xs = i[0]
+    ys = i[1]
+    zs = i[2]
+    r, g, b = xs/255, ys/255, zs/255
+    c = (r, g, b)
+    ax1.scatter(xs, ys, zs, c=c)
+
+ax1.set_xlabel('Red Value')
+ax1.set_ylabel('Green Value')
+ax1.set_zlabel('Blue Value')
+
+ax2 = fig.add_subplot(321)
+ax2.imshow(im, interpolation='nearest')
+ax2.axis("off")
+
+plt.show()
